5a57ccc8fe848db5512ae1ee2cd0fa88eebacd89
hashan0706 <Hashan.alwis97@gmail.com>
2021-10-01 22:38:32 +0530

    updated forum home

--- routes/forum.js
+++ routes/forum.js
@@ -9,7 +9,7 @@ router.get('/', function(req, res,) {
 
   connection.query("SELECT * FROM post", function(err, rows) {
     if(err) throw err;
-    console.log(rows);
+    // console.log(rows);
     res.render('forumHome', { post:rows });
 
   });
@@ -37,6 +37,80 @@ router.post('/addPost',function (req,res){
    
 
 });
+
+
+//direct to commet page 
+
+router.get('/forumPost/:pid', function(req,res){
+  
+  var postid= req.params.pid;
+
+  const post1=connection.query("SELECT * FROM post where pid = ?",[postid], function(err, rows) {
+    if(err) throw err;
+    res.render('forumPost', { post:rows });// edit 
+  
+  });
+  
+  const comment1=connection.query("SELECT * FROM comment where pid = ?",[postid], function(err, rows){
+    if(err) throw err;
+    console.log(rows);
+    // res.render('forumPost', { comment:rows });
+  })
+  
+    
+  
+});
+
+
+//add comment insert
+
+
+router.post('/forumPost/:pid',function (req,res){
+
+  // console.log(postid);
+  const commentData={
+    comment:req.body.comments,
+    pid:req.params.pid,
+    
+  };  
+  console.log(commentData);
+  
+  connection.query("insert into comment set ?", commentData,function (err,result){
+    if(err) throw err;
+    var postid= req.params.pid;
+    // var p=postid.concat('"')
+    // var url='"/forumHome/forumPost/';
+    // var redirectUrl=url.concat(p);
+    //  console.log('/forumHome/forumPost/'+ postid); 
+    res.redirect('/forumHome/forumPost/'+ postid);
+  });
+
+});
+
+
+//derect to user page
+
+//delete psot
+
+// router.get('/forumDelete/:pid', function(req,res){
+  
+//   var postid= req.params.pid;
+
+//   connection.query("delete * FROM post where pid = ?",[postid], function(err, rows) {
+//     if(err) throw err;
+//     res.redirect('/forumHome');// edit 
+  
+//   });
+     
+  
+// });
+
+
+
+
+
+
+
 
 
 
